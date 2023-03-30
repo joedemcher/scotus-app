b3fa4a582f5c95f69e6aa2cd29715fd1a4fd73d2
Joseph Demcher <23080198+joedemcher@users.noreply.github.com>
2023-03-30 19:06:37 -0400

    add pynecone app
    removes react app

---
+++ scotus_app/scotus_app.py
@@ -0,0 +1,82 @@
+from pcconfig import config
+
+import pynecone as pc
+
+# docs_url = "https://pynecone.io/docs/getting-started/introduction"
+# filename = f"{config.app_name}/{config.app_name}.py"
+courts = ["Roberts Court", "Warren Court", "Burger Court"]
+sides = ["Majority", "Minority"]
+
+class State(pc.State):
+    """The app state."""
+    judges = ["John Roberts", "Neil Gorsuch", "Elena Kagan", "Sonya Sotomayor"]
+    pass
+
+class Judges(pc.State):
+    judges = ["John Roberts", "Neil Gorsuch", "Elena Kagan", "Sonya Sotomayor"]
+
+def judge_selection(judge):
+        return pc.vstack(
+            pc.hstack(
+                pc.radio_group(
+                    sides,
+                ),
+                pc.image(
+                    src="https://upload.wikimedia.org/wikipedia/commons/5/58/Clarence_Thomas_official_SCOTUS_portrait.jpg",
+                    width="100px",
+                    height="auto",
+                    border="5px solid #555",
+                )
+            ),
+            pc.text(judge),
+        )
+
+def index() -> pc.Component:
+    return pc.center(
+        pc.vstack(
+            pc.heading("SCOTUS Decision Analyzer"),
+            pc.vstack(
+                pc.heading("Get started by choosing a court", font_size="1em"),
+                pc.select(
+                    courts,
+                    placeholder="Select a court",
+                ),
+                pc.button("Run analysis"),
+            ),
+            pc.hstack(
+                pc.vstack(
+                    pc.foreach(State.judges, judge_selection),
+                ),
+                pc.vstack(
+                    pc.foreach(State.judges, judge_selection),
+                ),
+            ),
+        ),
+    )
+
+# def index() -> pc.Component:
+#     return pc.center(
+#         pc.vstack(
+#             pc.heading("Welcome to Pynecone!", font_size="2em"),
+#             pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
+#             pc.link(
+#                 "Check out our docs!",
+#                 href=docs_url,
+#                 border="0.1em solid",
+#                 padding="0.5em",
+#                 border_radius="0.5em",
+#                 _hover={
+#                     "color": "rgb(107,99,246)",
+#                 },
+#             ),
+#             spacing="1.5em",
+#             font_size="2em",
+#         ),
+#         padding_top="10%",
+#     )
+
+
+# Add state and page to the app.
+app = pc.App(state=State)
+app.add_page(index)
+app.compile()
